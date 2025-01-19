138c9c4353941690f8bebf6eca84b4bb66c03a1e
Nishant Zoc <zocnishant@gmail.com>
2025-01-19 17:43:30 +0545

    Pusher-JS frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,27 @@
 import Sidebar from "./components/sidebar/Sidebar";
 import "./App.css";
 import Chat from "./components/chat/Chat";
+import { useEffect, useState } from "react";
+import Pusher from "pusher-js";
 
 const App = () => {
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    const pusher = new Pusher("9exxxxxxxxxxxx", {
+      cluster: "ap2",
+    });
+
+    const channel = pusher.subscribe("messahes");
+    channel.bind("inserted", (data) => {
+      setMessages([...messages, data]);
+    });
+
+    return () => {
+      channel.bind_all();
+      channel.unsubscribe();
+    };
+  }, [messages]);
   return (
     <div className="app">
       <section className="app_body">
